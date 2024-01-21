c79fd6cf4853bab49c7c48c3bb7a495592a20ec0
vini64theking <154025780+vini64theking@users.noreply.github.com>
2024-01-21 23:45:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ function clockProgram(){
     const now=new Date();
     let hours=now.getHours();
     const meridiem = hours >= 12 ? "PM" : "AM";
-    hours = hours % 12 || 12;
-    hours = hours.toString().padStart(2,0);
+    hours = (hours % 12 || 12).toString().padStart(2,0);
     const mins=now.getMinutes().toString().padStart(2,0);
     const sec=now.getSeconds().toString().padStart(2,0);
     const timestring=`${hours}:${mins}:${sec} ${meridiem}`;
